ef0f85f588478951a97e2e92ce39e36788a231f1
thirdsylim <thirdsylim@gmail.com>
2023-07-12 14:39:08 -0400

    initialize git

---
+++ cypress/e2e/my-spec.cy.js
@@ -0,0 +1,13 @@
+describe('Test Contact App', () => {
+
+  beforeEach(() => {
+    cy.visit('./contact_app.html')
+  })
+
+  it('Test if the application loads correctly', () => {
+    cy.get('h1.text-center').should('have.text', 'Contact List App');
+    cy.get('table tbody tr').should('have.length', 1)
+  })
+
+  // Add tests here
+});
